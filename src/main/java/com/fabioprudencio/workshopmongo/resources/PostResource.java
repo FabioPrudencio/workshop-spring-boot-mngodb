6ffefe27aca30d1f6569e55c25be1920b59a6d6e
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2022-06-04 18:56:28 -0300

    Consulta simples com @Query

--- src/main/java/com/fabioprudencio/workshopmongo/resources/PostResource.java
+++ src/main/java/com/fabioprudencio/workshopmongo/resources/PostResource.java
@@ -33,5 +33,12 @@ public class PostResource {
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> searchTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);		
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}	
 
 }
